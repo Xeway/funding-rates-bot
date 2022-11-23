d069aa8eed254dc342cf4d59e1f70ed409e7fcff
xeway <xeway@protonmail.com>
2022-11-23 22:22:01 +0100

    Comment

--- utils/main.go
+++ utils/main.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 )
 
+// Convert the string in parameter into a float64 and return it
 func StringToFloat(x string) float64 {
 	res, err := strconv.ParseFloat(x, 64)
 	if err != nil {
